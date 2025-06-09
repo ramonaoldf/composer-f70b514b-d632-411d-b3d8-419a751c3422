45c2afac996fd0aaed588db5bc6a4b786dd22bdf
RAMON ARAUJO OLIVEIRA <ramonaololiveira@gmail.com>
2025-06-09 03:04:36 -0300

    Vesion: 005.000.021.000

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -20,7 +20,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
 	 *
 	 * @var string
 	 */
-	const VERSION = '5.0.20';
+	const VERSION = '5.0.21';
 
 	/**
 	 * The base path for the Laravel installation.
@@ -70,6 +70,15 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
 	 * @var array
 	 */
 	protected $serviceProviders = array();
+
+	/**
+	 * A lookup of service providers by name.
+	 *
+	 * If the a provider is force-registered twice, only the first instance is included.
+	 *
+	 * @var array
+	 */
+	protected $registeredProviders = array();
 
 	/**
 	 * The names of the loaded service providers.
@@ -500,10 +509,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
 	{
 		$name = is_string($provider) ? $provider : get_class($provider);
 
-		return array_first($this->serviceProviders, function($key, $value) use ($name)
-		{
-			return $value instanceof $name;
-		});
+		return isset($this->registeredProviders[$name]) ? $this->registeredProviders[$name] : null;
 	}
 
 	/**
@@ -528,6 +534,11 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
 		$this['events']->fire($class = get_class($provider), array($provider));
 
 		$this->serviceProviders[] = $provider;
+
+		if ( ! isset($this->registeredProviders[$class]))
+		{
+			$this->registeredProviders[$class] = $provider;
+		}
 
 		$this->loadedProviders[$class] = true;
 	}
