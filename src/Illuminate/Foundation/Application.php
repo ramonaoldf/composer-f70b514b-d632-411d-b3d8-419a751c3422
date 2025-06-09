d3ae134db2ddb21de70d5da46477d2ded9abb65c
RAMON ARAUJO OLIVEIRA <ramonaololiveira@gmail.com>
2025-06-09 03:05:49 -0300

    Vesion: 005.000.024.000

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -20,7 +20,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
 	 *
 	 * @var string
 	 */
-	const VERSION = '5.0.23';
+	const VERSION = '5.0.24';
 
 	/**
 	 * The base path for the Laravel installation.
@@ -985,7 +985,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
 			'db'                   => 'Illuminate\Database\DatabaseManager',
 			'events'               => ['Illuminate\Events\Dispatcher', 'Illuminate\Contracts\Events\Dispatcher'],
 			'files'                => 'Illuminate\Filesystem\Filesystem',
-			'filesystem'           => 'Illuminate\Contracts\Filesystem\Factory',
+			'filesystem'           => ['Illuminate\Filesystem\FilesystemManager', 'Illuminate\Contracts\Filesystem\Factory'],
 			'filesystem.disk'      => 'Illuminate\Contracts\Filesystem\Filesystem',
 			'filesystem.cloud'     => 'Illuminate\Contracts\Filesystem\Cloud',
 			'hash'                 => 'Illuminate\Contracts\Hashing\Hasher',
